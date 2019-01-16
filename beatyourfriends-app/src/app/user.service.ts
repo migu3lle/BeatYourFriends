b1a62ffc0f9cc6d911540d8bebef783caa35d884
ChrissiSenger <46190019+ChrissiSenger@users.noreply.github.com>
2019-01-16 14:51:18 +0100

    Update user.service.ts

--- beatyourfriends-app/src/app/user.service.ts
+++ beatyourfriends-app/src/app/user.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Observable } from 'rxjs';
 import { User } from './user';
- 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
  
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private usersUrl = 'http://localhost:3000/users/';
+
+  private usersUrl = 'http://localhost:3000/api/user';
   constructor( 
-    private http: HttpClient
+    private http: HttpClient, private user: User
   ) { }
 
+
+  //CRUD operations
   getAllUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.usersUrl);
   }
 
-  getUser(id: number): Observable<User> {
-    return this.http.get<User>(this.usersUrl + id);
+  getUser(email: string) {
+    return this.http.get<User>(this.usersUrl + '/' + email);
   }
 
   updateUser(user: User): Observable<any> {
-    return this.http.put<void>(this.usersUrl + user.userid, user);
+    return this.http.put<void>(this.usersUrl, user);
   }
 
-  deleteUser(id: number) {
-    return this.http.delete<void>(this.usersUrl + id);
+  deleteUser(email: string) {
+    return this.http.delete<void>(this.usersUrl + '/'  + email);
   }
 }
