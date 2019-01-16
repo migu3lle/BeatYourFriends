a6cfad1e43b7844cf7a6edbf09b5efc89175ac62
ChrissiSenger <46190019+ChrissiSenger@users.noreply.github.com>
2019-01-16 15:08:09 +0100

    Update profil.component.ts

--- beatyourfriends-app/src/app/profil/profil.component.ts
+++ beatyourfriends-app/src/app/profil/profil.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { User } from '../user';
 import { UserService } from '../user.service';
- 
-import { ActivatedRoute, Params } from '@angular/router';
-import { Location } from '@angular/common';
+import { BrowserStorageService} from '../storage.service';
 
 @Component({
   selector: 'app-profil',
@@ -13,37 +12,73 @@ import { Location } from '@angular/common';
 })
 export class ProfilComponent implements OnInit {
 
-  user = new User() ;
+  user = new User();
+
+  //class to be shown in form in order to call update with 
+  //to input values
+  shownUser: User = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    userid: 1,
+    token: '',
+    password:''
+  };
+
   submitted = false;
   message: string;
 
   constructor(
     private userService: UserService,
-    private route: ActivatedRoute,
-    private location: Location
+    private browserStorage: BrowserStorageService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.userService.getUser(id)
-      .subscribe(user => this.user = user);
+    //get values from local storage
+    let email = this.browserStorage.get('email');
+    let lastn = this.browserStorage.get('lastname');
+    let firstn = this.browserStorage.get('firstname');
+
+    this.shownUser.firstname = firstn;
+    this.shownUser.lastname = lastn;
+    this.shownUser.email = email;
+
+    //get user from db
+    this.userService.getUser(email)
+      .subscribe(user => {
+      this.user = user;
+      });
   }
 
+//test method
+bStorage(): void {
+console.log(this.user);
+console.log(this.shownUser);
+}
+  
+  //call update method
   update(): void {
     this.submitted = true;
-    this.userService.updateUser(this.user)
-        .subscribe(() => this.message = "Dein Profil ist updated");
+    this.userService.updateUser(this.shownUser)
+        .subscribe(result => { this.message = "Dein Profil ist updated";
+        alert('Dein Profil wurde upgedated'!)});
   }
  
+  //call delete method
   delete(): void {
     this.submitted = true;
-    this.userService.deleteUser(this.user.userid)
-        .subscribe(()=> this.message = "Dein Profil wurde gelöscht");
-  }
- 
-  goBack(): void {
-    this.location.back();
+    this.userService.deleteUser(this.user[0].email)
+        .subscribe(result => {
+        this.message = "Dein Profil wurde gelöscht";
+        alert('Dein Profil wurde gelöscht!');
+        //log user out
+        this.browserStorage.remove('token');
+        this.router.navigate(['/login']);
+        });
   }
 
 }
+
+
 
