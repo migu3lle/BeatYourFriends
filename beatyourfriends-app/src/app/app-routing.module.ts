e3c416e89347e0cd5f84fa63136436ba479cebb4
ChrissiSenger <46190019+ChrissiSenger@users.noreply.github.com>
2019-01-16 14:58:01 +0100

    New Routes

--- beatyourfriends-app/src/app/app-routing.module.ts
+++ beatyourfriends-app/src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AuthGuardService as AuthGuard } from './auth-gard.service';
 import { ProfilComponent } from './profil/profil.component';
 import { FriendsComponent} from './friends/friends.component';
-import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { GameComponent} from './game/game.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { PasswordFgComponent } from './password-fg/password-fg.component';
+import { PasswordChangeComponent } from './password-change/password-change.component';
+import { PointsComponent } from './points/points.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent,},
-  {path: 'dashboard', component: ProfilComponent, canActivate: [AuthGuard]},
-  {path: 'friends', component: FriendsComponent, canActivate: [AuthGuard]},
-  {path: 'profil', component: ProfilComponent, canActivate: [AuthGuard] },
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  {path: 'users/:id', component: ProfilComponent,},
+  {path: 'login', component: LoginComponent },
+  {path: 'dashboard', component: FriendsComponent },
+  {path: 'friends', component: FriendsComponent },
+  {path: 'profil', component: ProfilComponent },
+  {path: 'game', component: GameComponent },
+  {path: 'quiz', component: QuizComponent },
+  {path: 'pwchange/:token', component: PasswordChangeComponent },
+  {path: 'pwfg', component: PasswordFgComponent },
+  {path: 'points', component: PointsComponent },
+  {path: '', redirectTo: '/login', pathMatch: 'full' },
+  //{path: 'users/:id', component: ProfilComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
