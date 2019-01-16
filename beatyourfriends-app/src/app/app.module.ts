a869e1cfdd49f5aa3e1a19c1f2195d3fd72a7414
ChrissiSenger <46190019+ChrissiSenger@users.noreply.github.com>
2019-01-16 14:56:34 +0100

    Update app module
    Add new modules

--- beatyourfriends-app/src/app/app.module.ts
+++ beatyourfriends-app/src/app/app.module.ts
@@ -1,32 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http'
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProfilComponent } from './profil/profil.component';
-import { UsersComponent } from './users/users.component';
 import { FooterComponent } from './footer/footer.component';
 import { FriendsComponent } from './friends/friends.component';
+import { User } from './user';
+import { GameComponent } from './game/game.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { PasswordFgComponent } from './password-fg/password-fg.component';
+import { PasswordChangeComponent } from './password-change/password-change.component';
+import { PointsComponent } from './points/points.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ProfilComponent,
-    UsersComponent,
     FooterComponent,
     FriendsComponent,
+    GameComponent,
+    QuizComponent,
+    PasswordFgComponent,
+    PasswordChangeComponent,
+    PointsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [User],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
