2daac58b0880158686d2d3c66c333b113849aa11
ChrissiSenger <46190019+ChrissiSenger@users.noreply.github.com>
2019-01-19 15:06:06 +0100

    Add files via upload

---
+++ beatyourfriends-app/src/app/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { User } from './user';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private usersUrl = 'http://localhost:3000/api/user';
+  constructor( 
+    private http: HttpClient, private user: User
+  ) { }
+
+
+  //CRUD operations
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
+
+  getUser(email: string) {
+    return this.http.get<User>(this.usersUrl + '/' + email);
+  }
+
+  getMailbyId(id: string) {
+    return this.http.post(this.usersUrl + '/' + id, id);
+  }
+
+  updateUser(user: User): Observable<any> {
+    return this.http.put<void>(this.usersUrl, user);
+  }
+
+  deleteUser(email: string) {
+    return this.http.delete<void>(this.usersUrl + '/'  + email);
+  }
+}
